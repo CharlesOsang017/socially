c39810a70b4d58313704b2ff1d1d308fb682903a
CharlesOsang017 <charlesosango02@gmail.com>
2026-04-17 11:28:18 +0300

    Add login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -57,8 +57,32 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send('login route')
+export const login = async(req, res) => {
+    try {
+        const {password, username} = req.body;
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || '')
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: 'Invalid username or password'})
+        }
+
+        generateTokenAndSetCookie(user._id, res)
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            email: user.email,
+            followers: user.followers,
+            following: user.following,
+            profileImg: user.profileImg,
+            coverImg: user.coverImg,
+        })
+
+    } catch (error) {
+        console.log('Error in login controller', error.message)
+        res.status(500).json({error: error.message})
+    }
 }
 
 export const logout = (req, res) => {
