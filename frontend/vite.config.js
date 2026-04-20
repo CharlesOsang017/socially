b5a250a88f49f005a3b909f1cbd2fa48300ba780
CharlesOsang017 <charlesosango02@gmail.com>
2026-04-20 05:39:55 +0300

    Finish frontend UI designs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
+// import daisyui from "daisyui";
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+//   daisyui: {
+// 		themes: [
+// 			"light",
+// 			{
+// 				black: {
+// 					primary: "rgb(29, 155, 240)",
+// 					secondary: "rgb(24, 24, 24)",
+// 				},
+// 			},
+// 		],
+// 	},
 })
